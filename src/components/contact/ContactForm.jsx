00bbfbd0744cc56a0fa673007a769eb4964d9472
Paul Johns <johnspaulm@gmail.com>
2024-01-19 16:35:08 -0600

    contact form change and submit functions working

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -3,19 +3,49 @@ import { useState } from "react";
 const ContactForm = () => {
   const [formData, setFormData] = useState({});
 
+  const updateContactForm = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    console.log("form data", formData);
+  };
+
+  const submitContactForm = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" id="name" placeholder="Your Name" required></input>
+      <form onSubmit={submitContactForm}>
+        <input
+          type="text"
+          id="name"
+          placeholder="Your Name"
+          required
+          onChange={updateContactForm}
+        ></input>
         <textarea
           id="message"
           placeholder="Message"
           rows="10"
           cols="50"
           required
+          onChange={updateContactForm}
         ></textarea>
-        <input type="text" id="email" placeholder="Your Email"></input>
-        <input type="text" id="phone" placeholder="Your Contact Number"></input>
+        <input
+          type="email"
+          id="email"
+          placeholder="Your Email"
+          onChange={updateContactForm}
+        ></input>
+        <input
+          type="text"
+          id="phone"
+          placeholder="Your Contact Number"
+          onChange={updateContactForm}
+        ></input>
         <button>Submit</button>
       </form>
     </div>
