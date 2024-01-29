1c1a09f6211782e0adebcffbcc7b3b50303a578a
Paul Johns <johnspaulm@gmail.com>
2024-01-28 20:26:10 -0600

    more project card styling

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -3,7 +3,7 @@ const ProjectCard = (props) => {
     <project-card>
       <h3 className="project-title">{props.title}</h3>
       <p className="project-description">{props.description}</p>
-      <img src={props.image} />
+      <img src={props.image} className="project-hero" />
     </project-card>
   );
 };
